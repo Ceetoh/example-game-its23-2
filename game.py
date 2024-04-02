8d67421356f2d91a8e0105d72afb519336d9a9d3
Ceetoh <martin.pihlapuu@voco.ee>
2024-04-02 14:26:49 +0300

    As a player I can see the game window [Delivers #187353233]

--- game.py
+++ game.py
@@ -1,10 +1,19 @@
 import pygame
+from settings import Settings
 
 pygame.init()
-ekraan = pygame.display.set_mode((800, 500))
+gm_settings = Settings()
+
+
+
+screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+pygame.display.set_caption(gm_settings.caption)
+
+
 running = True
 
 while running:
+    screen.fill(gm_settings.bg_color)            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
