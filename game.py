e925d7e8ca604d4854ab94c8f2b3c78a1304f629
Ceetoh <martin.pihlapuu@voco.ee>
2024-04-16 13:14:37 +0300

    As a player i can see and move my character (player) at screen [Delivers #187353586]

--- game.py
+++ game.py
@@ -1,23 +1,20 @@
 import pygame
 from settings import Settings
+from player import Player
+import game_function as gf
 
-pygame.init()
-gm_settings = Settings()
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
 
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
 
+    player = Player(screen)
+    
+    while True:
+        gf.check_events(player)
+        player.update()
+        gf.uptade_screen(gm_settings, screen, player)
 
-screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
-pygame.display.set_caption(gm_settings.caption)
-
-
-running = True
-
-while running:
-    screen.fill(gm_settings.bg_color)            
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.flip()
-
-pygame.quit()
+run_game()
